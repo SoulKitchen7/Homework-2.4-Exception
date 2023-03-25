74cb6660c603a3facfdd8760117f3e71d440245e
SoulKitchen7 <whiskygogo@mail.ru>
2023-03-25 11:36:54 +0300

    Another commit. Homework 2.4 done.

--- src/main/java/pro/sky/java/course2/exception/service/LoginServiceImpl.java
+++ src/main/java/pro/sky/java/course2/exception/service/LoginServiceImpl.java
@@ -1,9 +1,7 @@
 package pro.sky.java.course2.exception.service;
-
 import org.springframework.stereotype.Service;
 import pro.sky.java.course2.exception.exception.WrongLoginException;
 import pro.sky.java.course2.exception.exception.WrongPasswordException;
-
 @Service
 public class LoginServiceImpl implements LoginService {
     @Override
